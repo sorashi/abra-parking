d0fb33411bd81e8ff00cee8a949faa10d75a5847
Dennis Prazak <prazak.dennis@gmail.com>
2021-04-16 16:40:34 +0200

    Allow logging from everyone for now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,43 @@ import LoginForm from "./components/LoginForm";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container, Row} from "react-bootstrap";
 
-export default class App extends Component {
+type AppState = {
+    isLoggedIn: boolean
+    username: string | null,
+    password: string | null
+}
+
+export default class App extends Component<{}, AppState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            isLoggedIn: false,
+            username: null,
+            password: null
+        }
+        this.handleLoginFormSubmit = this.handleLoginFormSubmit.bind(this)
+    }
     handleLoginFormSubmit(username: string, password: string) {
         console.log('logging in as', username, password)
+        this.setState(_ => ({
+            isLoggedIn: true,
+            username: username,
+            password: password
+        }))
     }
 
     render() {
+        let row = <Row className="justify-content-center">
+            <LoginForm onSubmit={this.handleLoginFormSubmit}/>
+        </Row>;
+        if(this.state.isLoggedIn) {
+            row = <Row className="justify-content-center">
+                <span>Logged in as {this.state.username}</span>
+            </Row>
+        }
         return (
             <Container style={{padding: "10px"}}>
-                <Row className="justify-content-center">
-                    <LoginForm onSubmit={this.handleLoginFormSubmit}/>
-                </Row>
+                {row}
             </Container>
         );
     }
